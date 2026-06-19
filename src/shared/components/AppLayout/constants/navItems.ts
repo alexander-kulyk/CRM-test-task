dba2dd2825c02559dd592f8caba1391b2b88ea22
Oleksandr Kulyk <oleku@smart-it.com>
2026-06-19 19:54:27 +0300

    refactor(navItems): clean up navigation items by removing unused entries and fixing formatting

--- src/shared/components/AppLayout/constants/navItems.ts
+++ src/shared/components/AppLayout/constants/navItems.ts
@@ -1,17 +1,9 @@
 export interface INavItem {
-  label: string
-  to?: string
+  label: string;
+  to?: string;
 }
 
 export const NAV_ITEMS: INavItem[] = [
   { label: 'Home', to: '/' },
-  { label: 'Dashboard' },
-  { label: 'Inbox' },
-  { label: 'Products' },
-  { label: 'Invoices' },
-  { label: 'Customers' },
-  { label: 'Chat Room' },
   { label: 'Calendar', to: '/calendar' },
-  { label: 'Help Center' },
-  { label: 'Settings' },
-]
+];
